dc3080cb2e6384329627ee4f2fa42586135a6f09
Ivan Blagoychev <ivanblagoychev@gmail.com>
2017-04-15 13:10:03 +0300

    BookShopSystem Homework

---
+++ 06. Entity Relations Advanced/BookShopSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using BookShopSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new BookShopContext();
+            //ctx.Database.Initialize(true);
+
+            var books = ctx.Books.Take(3).ToList();
+            books[0].RelatedBooks.Add(books[1]);
+            books[1].RelatedBooks.Add(books[2]);
+            books[2].RelatedBooks.Add(books[0]);
+            ctx.SaveChanges();
+
+            var booksFromQuery = ctx.Books
+                .Take(3)
+                .Select(s => new
+                {
+                    name = s.Title,
+                    relatedBooks = s.RelatedBooks.Select(n=>n.Title)
+                });
+            foreach (var book in booksFromQuery)
+            {
+                Console.WriteLine("--{0}", book.name);
+                foreach (var r in book.relatedBooks)
+                {
+                    Console.WriteLine(r);
+                }
+            }
+        }
+    }
+}
